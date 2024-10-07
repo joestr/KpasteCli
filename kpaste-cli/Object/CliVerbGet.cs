27a9f86cf09ae6f8d5bb6ebd649bc61d011b1e0e
Joel Strasser <j.strasser@leeb-balkone.com>
2024-10-07 11:37:00 +0200

    fix: move CliVerbNew and CliVerGet to correct namespace

--- kpaste-cli/Object/CliVerbGet.cs
+++
@@ -1,9 +0,0 @@
-using CommandLine;
-
-namespace kpaste_cli.Objects;
-
-[Verb("get", HelpText = "Get a paste.")]
-public class CliVerbGet
-{
-    
-}
---
+++ kpaste-cli/Objects/CliVerbGet.cs
@@ -0,0 +1,13 @@
+using CommandLine;
+
+namespace kpaste_cli.Objects;
+
+[Verb("get", HelpText = "Get a paste.")]
+public class CliVerbGet
+{
+    [Option("url", Required = true, HelpText = "The URL of the paste.")]
+    public string Url { get; set; }
+
+    [Option("password", Required = false, Default = "", HelpText = "The password the paste has been encrypted with.")]
+    public string Password { get; set; } = "";
+}

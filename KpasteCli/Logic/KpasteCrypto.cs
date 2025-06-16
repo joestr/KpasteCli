3cfec1c8caba0f77fdc4a9f8dbe3c7902793dbba
Joel Strasser <j.strasser@leeb-balkone.com>
2025-06-16 14:14:28 +0200

    fix: remove strings and use bytes
    Finally fixes the issue where the encrypted pastes from the WebUI could not be decrypted.

--- KpasteCli/Logic/KpasteCrypto.cs
+++ KpasteCli/Logic/KpasteCrypto.cs
@@ -16,9 +16,9 @@ namespace KpasteCli.Logic
         private static readonly int KeySize = 256;
         private static readonly int TagSize = 128;
 
-        private readonly string _key;
-        private readonly string _vector;
-        private readonly string _salt;
+        private readonly byte[] _key;
+        private readonly byte[] _vector;
+        private readonly byte[] _salt;
 
         public class KpasteEncryptionResultDto
         {
@@ -31,8 +31,8 @@ namespace KpasteCli.Logic
         public KpasteCrypto(string key, string vector, string salt)
         {
             this._key = FromBase58(key);
-            this._vector = ArrayBufferToString(Convert.FromBase64String(vector));
-            this._salt = ArrayBufferToString(Convert.FromBase64String(salt));
+            this._vector = Convert.FromBase64String(vector);
+            this._salt = Convert.FromBase64String(salt);
         }
         
         public KpasteCrypto()
@@ -50,8 +50,8 @@ namespace KpasteCli.Logic
             var result = new KpasteEncryptionResultDto()
             {
                 Key = ToBase58(this._key),
-                Vector = Convert.ToBase64String(StringToArrayBuffer(this._vector)),
-                Salt = Convert.ToBase64String(StringToArrayBuffer(this._salt)),
+                Vector = Convert.ToBase64String(this._vector),
+                Salt = Convert.ToBase64String(this._salt),
                 Message = message
             };
             return result;
@@ -81,16 +81,15 @@ namespace KpasteCli.Logic
 
             IBlockCipher cipher = new AesEngine();
             KeyParameter keyParam = new KeyParameter(derivedKey);
-            AeadParameters keyParamAead = new AeadParameters(keyParam, TagSize, StringToArrayBuffer(this._vector), new byte[0]);
+            AeadParameters keyParamAead = new AeadParameters(keyParam, TagSize, this._vector, new byte[0]);
             GcmBlockCipher cipherMode = new GcmBlockCipher(cipher);
             cipherMode.Init(true, keyParamAead);
             int outputSize = cipherMode.GetOutputSize(compressedPlainTextBytes.Length);
             byte[] cipherTextData = new byte[outputSize];
             int result = cipherMode.ProcessBytes(compressedPlainTextBytes, 0, compressedPlainTextBytes.Length, cipherTextData, 0);
             cipherMode.DoFinal(cipherTextData, result);
-            encryptedBytes = cipherTextData;
 
-            var resultString = Convert.ToBase64String(encryptedBytes);
+            var resultString = Convert.ToBase64String(cipherTextData);
 
             return resultString;
         }
@@ -102,7 +101,7 @@ namespace KpasteCli.Logic
 
             IBlockCipher cipher = new AesEngine();
             KeyParameter keyParam = new KeyParameter(derivedKey);
-            AeadParameters keyParamAead = new AeadParameters(keyParam, TagSize, StringToArrayBuffer(this._vector), new byte[0]);
+            AeadParameters keyParamAead = new AeadParameters(keyParam, TagSize, this._vector, new byte[0]);
             GcmBlockCipher cipherMode = new GcmBlockCipher(cipher);
             cipherMode.Init(false, keyParamAead);
             var outputSize = cipherMode.GetOutputSize(cipherTextBytes.Length);
@@ -129,56 +128,46 @@ namespace KpasteCli.Logic
             byte[] newKeyBytes;
             if (password.Length > 0)
             {
-                var passwordBytes = StringToArrayBuffer(password);
-                var keyBytes = StringToArrayBuffer(this._key);
+                var passwordBytes = Encoding.Default.GetBytes(password);
+                var keyBytes = this._key;
                 newKeyBytes = new byte[keyBytes.Length + passwordBytes.Length];
                 keyBytes.CopyTo(newKeyBytes, 0);
                 passwordBytes.CopyTo(newKeyBytes, keyBytes.Length);
             }
             else
             {
-                var keyBytes = StringToArrayBuffer(this._key);
+                var keyBytes = this._key;
                 newKeyBytes = keyBytes;
             }
 
-            var saltBytes = StringToArrayBuffer(this._salt);
+            var saltBytes = this._salt;
 
-            return Rfc2898DeriveBytes.Pbkdf2(newKeyBytes, saltBytes, IterationCount, HashAlgorithmName.SHA256,
+            return Rfc2898DeriveBytes.Pbkdf2(
+                newKeyBytes,
+                saltBytes, 
+                IterationCount,
+                HashAlgorithmName.SHA256,
                 KeySize/8);
         }
 
-        private string ToBase58(string input)
+        private string ToBase58(byte[] input)
         {
-            return Base58.Bitcoin.Encode(StringToArrayBuffer(input));
+            return Base58.Bitcoin.Encode(input);
         }
 
-        private string FromBase58(string input)
+        private byte[] FromBase58(string input)
         {
-            return ArrayBufferToString(Base58.Bitcoin.Decode(input));
+            return Base58.Bitcoin.Decode(input);
         }
         
         private string ArrayBufferToString(byte[] messageArray)
         {
-            return Encoding.UTF8.GetString(messageArray);
-
-            // This somehow has problems with text conversion.
-            //var message = "";
-            //for (var i = 0; i < messageArray.Length; i += 1) {
-            //    message += (char)messageArray[i];
-            //}
-            //return message;
+            return Encoding.Default.GetString(messageArray);
         }
 
         private byte[] StringToArrayBuffer(string message)
         {
-            return Encoding.UTF8.GetBytes(message);
-
-            // This somehow has problems with text conversion.
-            //byte[] messageArray = new byte[message.Length];
-            //for (var i = 0; i < message.Length; i += 1) {
-            //    messageArray[i] = (byte)message[i];
-            //}
-            //return messageArray;
+            return Encoding.Default.GetBytes(message);
         }
 
         private string Utf16ToUtf8(string message)
@@ -195,19 +184,12 @@ namespace KpasteCli.Logic
             //return Encoding.Unicode.GetString(Encoding.UTF8.GetBytes(message));
         }
 
-        private string GetRandomBytes(int length)
+        private byte[] GetRandomBytes(int length)
         {
-            var result = "";
-
             var bytes = new byte[length];
             Random.Shared.NextBytes(bytes);
 
-            for (var i = 0; i < bytes.Length; i += 1)
-            {
-                result += (char)bytes[i];
-            }
-
-            return result;
+            return bytes;
         }
     }
 }
